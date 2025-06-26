e2705e1a790fec47876f0ec738b36b46e25443ae
Sefger <sefger2006@gmail.com>
2025-06-26 22:41:48 +0300

    i64 to i32

--- src/models.rs
+++ src/models.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, sqlx::FromRow)]
 pub struct Task{
-    pub id: i64,
+    pub id: i32,
     pub title: String,
     pub completed: bool
 }
